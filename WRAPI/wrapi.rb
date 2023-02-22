cfcfaad7fc02d395fe3f5cc7d6135e4cf2c1c4de
CiBri0 <75755417+CiBri0@users.noreply.github.com>
2023-02-22 23:45:33 +0100

    Update wrapi.rb

--- WRAPI/wrapi.rb
+++ WRAPI/wrapi.rb
@@ -18,6 +18,3 @@ module WRAPI
         end
     end
 end
-
-
-WAPI::Background.replace_from_web("https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/ruby/ruby.png")
